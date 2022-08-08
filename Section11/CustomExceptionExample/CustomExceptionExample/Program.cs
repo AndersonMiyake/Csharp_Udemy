d5689558adc9091d9c063d1d144ce167f765133d
Anderson Miyake <afmyk@hotmail.com>
2022-08-08 12:53:41 -0300

    Section 11 and 12

---
+++ Section11/CustomExceptionExample/CustomExceptionExample/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using CustomExceptionExample.Entities;
+using CustomExceptionExample.Entities.Exceptions;
+
+namespace CustomExceptionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 152. Criando exceções personalizadas - PARTE 1 - Very Bad solution
+            //Console.Write("Room Number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.Write("Check-in date (dd/MM/yyyy): ");
+            //DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            //Console.Write("Check-out date (dd/MM/yyyy): ");
+            //DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            //if (checkOut <= checkIn)
+            //{
+            //    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            //}
+            //else
+            //{
+            //    Reservation reservation = new Reservation(number, checkIn, checkOut);
+            //    Console.WriteLine("Reservation: " + reservation);
+
+            //    Console.WriteLine();
+            //    Console.WriteLine("Enter data to update the reservation:");
+            //    Console.Write("Check-in date (dd/MM/yyyy): ");
+            //    checkIn = DateTime.Parse(Console.ReadLine());
+            //    Console.Write("Check-out date (dd/MM/yyyy): ");
+            //    checkOut = DateTime.Parse(Console.ReadLine());
+
+            //    DateTime now = DateTime.Now;
+            //    if (checkIn < now || checkOut < now)
+            //    {
+            //        Console.WriteLine("Erro in reservation: Reservation dates for update must be future dates");
+            //    }
+            //    else if (checkOut <= checkIn)
+            //    {
+            //        Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            //    }
+            //    else
+            //    {
+            //        reservation.UpdateDates(checkIn, checkOut);
+            //        Console.WriteLine("Reservation: " + reservation);
+            //    }
+            //}
+            #endregion
+
+            #region 153. Criando exceções personalizadas - PARTE 2 - Bad Solution
+            //Console.Write("Room Number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.Write("Check-in date (dd/MM/yyyy): ");
+            //DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            //Console.Write("Check-out date (dd/MM/yyyy): ");
+            //DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            //if (checkOut <= checkIn)
+            //{
+            //    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            //}
+            //else
+            //{
+            //    Reservation reservation = new Reservation(number, checkIn, checkOut);
+            //    Console.WriteLine("Reservation: " + reservation);
+
+            //    Console.WriteLine();
+            //    Console.WriteLine("Enter data to update the reservation:");
+            //    Console.Write("Check-in date (dd/MM/yyyy): ");
+            //    checkIn = DateTime.Parse(Console.ReadLine());
+            //    Console.Write("Check-out date (dd/MM/yyyy): ");
+            //    checkOut = DateTime.Parse(Console.ReadLine());
+
+            //    string error = reservation.UpdateDates(checkIn, checkOut);
+
+            //    if (error != null)
+            //    {
+            //        Console.WriteLine("Error in reservation: " + error);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Reservation: " + reservation);
+            //    }
+            //}
+            #endregion
+
+            #region 154. Criando exceções personalizadas - PARTE 3 - Good Solution
+            try
+            {
+                Console.Write("Room Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+            #endregion
+        }
+    }
+}
---
+++ Section11/ExceptionsPracticeExercise/ExceptionsPracticeExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using ExceptionsPracticeExercise.Entities;
+using ExceptionsPracticeExercise.Entities.Exceptions;
+
+namespace ExceptionsPracticeExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+                Console.Write("\nEnter the amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+                Console.Write("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Domain error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
